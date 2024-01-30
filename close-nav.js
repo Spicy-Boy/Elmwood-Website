06bbf9ef432fdc9d58c9046e9841a8fb4f164d0e
Spicy-Boy <46385369+Spicy-Boy@users.noreply.github.com>
2024-01-30 13:04:42 -0500

    Mobile Layout Draft w/ Hamburger

---
+++ close-nav.js
@@ -0,0 +1,7 @@
+// DESIRED FUNCTIONALITY: 
+/*
+
+    I want the #nav-checkbox to become unchecked when I click outside of the nav box. Basically, auto-close the menu when you click outside it.
+
+*/
+

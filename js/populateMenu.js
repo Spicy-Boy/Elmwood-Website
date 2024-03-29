ce484b9572460cddc6e62a1b1db85fc0b394b618
Spicy-Boy <46385369+Spicy-Boy@users.noreply.github.com>
2024-03-28 23:31:40 -0400

    Testing http-server for local JSON testing

--- js/populateMenu.js
+++ js/populateMenu.js
@@ -1,3 +1,16 @@
 //DOM object where menu sections are placed
 let menuWrapper = document.getElementById('menu-wrapper');
 
+// grab the JSON file
+fetch("js/menu.JSON")
+    .then((res)=>{
+        if (!res.ok)
+        {
+            throw new Error("Could not get menu.JSON");
+        }
+
+        return res.json();
+    })
+    .then((data)=>{
+        console.log(data);
+    });

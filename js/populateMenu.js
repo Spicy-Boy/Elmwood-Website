1be1b9365e6a6454957ef3c1318d2a3b80eda9fd
Spicy-Boy <46385369+Spicy-Boy@users.noreply.github.com>
2024-03-29 00:18:42 -0400

    Began populateMenu script, fetches from JSON

--- js/populateMenu.js
+++ js/populateMenu.js
@@ -11,6 +11,46 @@ fetch("js/menu.JSON")
 
         return res.json();
     })
-    .then((data)=>{
-        console.log(data);
+    .then((menuObj)=>{
+        console.log(menuObj);
+
+        for (let sectionKey in menuObj)
+        {
+            menuSection = menuObj[sectionKey];
+            // console.log(menuSection);
+
+            let newSection = document.createElement('section');
+
+            let h1 = document.createElement('h1');
+            h1.classList.add("menu-section-header");
+            h1.textContent = menuSection.sectionTitle;
+            newSection.appendChild(h1);
+
+            if (menuSection.sectionDescription != "")
+            {
+                let h2 = document.createElement('h2')
+                h2.classList.add("menu-section-description");
+                h2.textContent = menuSection.sectionDescription;
+                newSection.appendChild(h2);
+            }
+
+            let divEntry = document.createElement('div');
+            divEntry.classList.add("menu-entry");
+            newSection.appendChild(divEntry);
+
+            let titleDiv = document.createElement('div');
+            titleDiv.classList.add("menu-flex-row-75-25");
+            // titleDiv.classList.add("title-with-price-wrapper"); //not implemented yet
+
+            for(item of menuSection.array)
+            {
+
+            }
+
+        }
     });
+
+function createSection()
+{
+
+}
